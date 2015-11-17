b61eaeb753b135e7dd3023e0289d01d9fa0b532d
David Daeschler <david.daeschler@gmail.com>
2015-11-17 15:34:55 -0600

    Try a different method for fdisk

--- format-and-mount-volume.sh
+++ format-and-mount-volume.sh
@@ -1,19 +1,3 @@
-# to create the partitions programatically (rather than manually)
-# we're going to simulate the manual input to fdisk
-# The sed script strips off all the comments so that we can
-# document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
-# line terminated with a newline to take the fdisk default.
-sed -e 's/\t\([\+0-9a-zA-Z]*\)[ \t].*/\1/' << EOF | fdisk "/dev/xvdb"
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk
-    # default - entire disk
-  w # write the partition table
-  q # and we're done
-EOF
-
+(echo o; echo n; echo p; echo 1; echo ; echo; echo w) | fdisk "/dev/xvdb"
 mkfs.ext4 /dev/xvdb1
 mount /dev/xvdb1
